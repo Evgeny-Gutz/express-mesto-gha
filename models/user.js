cdd41abf98873db05e22a5bc91a1b4a2da7a2de5
Evgeny Gutz <evgeny.gutz@yandex.ru>
2023-03-03 00:37:01 +0300

    refactor: Автотесты: Валидация имени меньше 2х символов.

--- models/user.js
+++ models/user.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30
+  name: { // у пользователя есть имя — опишем требования к имени в схеме:
+
+    type: String, // имя — это строка
+
+    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
+
+    minlength: 2, // минимальная длина имени — 2 символа
+
+    maxlength: 30, // а максимальная — 30 символов
+
   },
   about: {
     type: String,
