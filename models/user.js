5f436cbfcda27435c8521ea2608dccd2c9383e4d
Evgeny Gutz <evgeny.gutz@yandex.ru>
2023-03-15 14:24:16 +0300

    feature: Добавьте email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    requred: true,
+    minlength: 8,
+  },
   name: {
     type: String,
     required: true,
